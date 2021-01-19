5ea7f9a540a1956e5d1bb33facd65c64ba956c24
Murat Mirgün Ercan <muratmirgunercan225@gmail.com>
2021-01-19 22:52:01 +0300

    init

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+/* eslint-disable react/require-render-return */
+import React from 'react';
+
+class Comp extends React.Component {
+
+  render() {
+    <p x-text="count"></p>;
+  }
+
+ 
+  
+}
+
+export default Comp;
