390d069c3e3bfcf8bca0ec1d4d3f48e838158c8e
Murat Mirgün Ercan <muratmirgunercan225@gmail.com>
2021-02-13 01:15:50 +0300

    card componenti eklendi

---
+++ client/src/components/Card.vue
@@ -0,0 +1,19 @@
+<script>
+import Scard from './SingleCard'
+export default {
+  components: {
+    Scard
+  },
+}
+</script>
+<template>
+  <div>
+  <div class="a"></div>
+   <div><Scard/></div>
+   
+  </div>
+</template>
+
+<style scoped>
+
+</style>
